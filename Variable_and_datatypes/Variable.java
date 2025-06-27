db293da6d020191c35f6eb34324bee3b20aefd31
rahulkundelwalll <pooransherrahul@gmail.com>
2025-06-27 19:16:14 +0530

    variable

---
+++ Variable_and_datatypes/Variable.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Variable {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter two number by space");
+        int a = s.nextInt();
+        System.out.println("value of a is : "+a);
+        int b = s.nextInt();
+        System.out.println("value of a is : "+b);
+        System.out.println(a+b);
+         System.out.println(a-b);
+        
+    }
+}
