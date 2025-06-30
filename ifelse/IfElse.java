ebfef0be2a548bd6f65055a2c73b40adf7415533
rahulkundelwalll <pooransherrahul@gmail.com>
2025-06-30 20:49:34 +0530

    ifelse

---
+++ ifelse/IfElse.java
@@ -0,0 +1,17 @@
+class IfElse{
+    public static void main(String[] args) {
+        boolean rainy = false;
+        boolean sunny = false;
+        if(rainy){
+           
+            System.out.println("kal mai bala kila jaunga");
+        }
+        else if(sunny){
+            System.out.println("kal mai silished jaunga");
+        }
+        else{
+            System.out.print("verna mai ghar rahunga");
+        }
+        System.out.println("end");
+    }
+}
