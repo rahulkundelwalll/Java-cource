5e7c7ce90ff9c0d2db3397ca45c823b1e4f64919
rahulkundelwalll <pooransherrahul@gmail.com>
2025-07-03 22:31:39 +0530

    loop

---
+++ loop/For.java
@@ -0,0 +1,49 @@
+// package loop;
+
+public class For {
+    public static void main(String[] args) {
+        // ** while **
+        // int i = 0;
+        // int ans = 0;
+        // while(i<=5){
+            
+        //     i++;
+            
+        //     ans = ans + i; 
+        // }
+        // System.out.println(ans);
+
+
+        // int i = 1;
+        // int fact = 1;
+
+        // while(i<=5){
+        //     fact = fact*i;  // i = 6 , fact = 120;
+        //     i++;
+
+        // }
+        // System.out.println(fact);
+
+
+
+        // **for loop **
+        // int sum = 0;
+        // for(int i = -1;i<=5;i++){
+        //     sum= sum+i;
+
+        // }
+        // System.out.println(sum);
+
+
+        // ** do while **
+        // int i = 0;
+        //  do{
+        //     i++;
+        //     System.out.println(i);
+        //  }while(i<5);
+
+        
+         
+        
+    }
+}
